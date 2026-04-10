558a1001a749135369389b46d3f2c2f65894cd5b
vinigodoy <vinigodoy@gmail.com>
2026-04-10 16:03:35 -0300

    Bootstrapper

--- src/main/kotlin/br/pucpr/authserver/users/UserController.kt
+++ src/main/kotlin/br/pucpr/authserver/users/UserController.kt
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/users")
 class UserController(val service: UserService) {
     @GetMapping
-    fun list(@RequestParam sortDir: String? = null, @RequestParam role: String? = null) =
-        if (role != null) {
+    fun list(
+        @RequestParam sortDir: String? = null,
+        @RequestParam role: String? = null
+    ) = if (role != null) {
             val users = service.findByRole(role)
             ResponseEntity.ok(users)
         } else {
@@ -20,24 +22,31 @@ class UserController(val service: UserService) {
         }
 
     @PostMapping
-    fun insert(@RequestBody user: User) =
-        service.insert(user)
+    fun insert(
+        @RequestBody user: User
+    ) = service.insert(user)
             ?.let { ResponseEntity.status(HttpStatus.CREATED).body(it) }
             ?: ResponseEntity.badRequest().build()
 
     @GetMapping("/{id}")
-    fun getById(@PathVariable id: Long) =
-        service.findByIdOrNull(id)
+    fun getById(
+        @PathVariable id: Long
+    ) = service.findByIdOrNull(id)
             ?.let { ResponseEntity.ok(it) }
             ?: ResponseEntity.notFound().build()
 
     @DeleteMapping("/{id}")
-    fun delete(@PathVariable id: Long): ResponseEntity<Void> =
+    fun delete(
+        @PathVariable id: Long
+    ): ResponseEntity<Void> =
         if (service.delete(id)) ResponseEntity.ok().build()
         else ResponseEntity.notFound().build()
 
     @PutMapping("/{id}/roles/{role}")
-    fun grant(@PathVariable id: Long, @PathVariable role: String): ResponseEntity<Void> =
+    fun grant(
+        @PathVariable id: Long,
+        @PathVariable role: String
+    ): ResponseEntity<Void> =
         service.addRole(id, role)
             ?.let { if (it) ResponseEntity.ok().build() else ResponseEntity.noContent().build() }
             ?: ResponseEntity.badRequest().build()
